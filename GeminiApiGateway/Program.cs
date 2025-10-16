01551aebcb4569359c9682b286ca66d184799ca0
Hubert Dumas <hubertd@gmail.com>
2025-10-17 07:14:51 +1100

    clean up

--- GeminiApiGateway/Program.cs
+++ GeminiApiGateway/Program.cs
@@ -3,6 +3,10 @@ using Ocelot.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Force .NET to prefer IPv4 for localhost resolution
+// This fixes the issue where Docker containers don't properly handle IPv6
+AppContext.SetSwitch("System.Net.DisableIPv6", true);
+
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -17,26 +21,15 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
+// DO NOT use UseHttpsRedirection() with Ocelot when downstream services are HTTP
+// The gateway will still accept HTTPS connections (configured in launchSettings.json)
+// but won't interfere with Ocelot's ability to proxy to HTTP downstream services
 
-var summaries = new[]
+// Optional: Use HSTS in production for security (only if all clients support HTTPS)
+if (!app.Environment.IsDevelopment())
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    app.UseHsts();
+}
 
 await app.UseOcelot();
 
